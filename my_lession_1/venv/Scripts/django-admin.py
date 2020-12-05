27ca259126b6d6af5e6d06f6895f2ce3fba7f23a
Mazhit <mig7676@yandex.ru>
2020-12-05 20:07:41 +0300

    Version2toLector
    Two version my Lession 1

---
+++ my_lession_1/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user5\desktop\lession\django\my_lession_1\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
